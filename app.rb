0f8c7421ba3ea9507467b6da196d84998ed17b7e
Dr Nic Williams <drnicwilliams@gmail.com>
2011-09-05 23:10:42 -0700

    add a root url

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require "sinatra/base"
 require "open-uri"
 
 class FetchTweets < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
+  
   get '/:user' do
     open("http://search.twitter.com/search.json?q=#{params[:user]}")
   end
